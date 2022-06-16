89a6ba52cd181fdc4fc7f8dddde40de542e79991
AyushSalunke <66405784+AyushSalunke@users.noreply.github.com>
2022-06-16 10:25:40 +0530

    Simchanges

--- square-auto/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ square-auto/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,12 +8,22 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
 import com.revrobotics.CANSparkMax;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -22,6 +32,7 @@ import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.VecBuilder;
 // import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
@@ -32,9 +43,9 @@ import edu.wpi.first.wpilibj.SPI;
 public class DriveTrain extends SubsystemBase {
 
   CANSparkMax leftmotorfront;
-   CANSparkMax leftmotorback;
+  CANSparkMax leftmotorback;
   CANSparkMax rightmotorfront;
-   CANSparkMax rightmotorback;
+  CANSparkMax rightmotorback;
   // SpeedControllerGroup leftmotors;
   DifferentialDrive drive;
   DifferentialDriveOdometry drive2;
@@ -44,6 +55,8 @@ public class DriveTrain extends SubsystemBase {
   RelativeEncoder encrightf;
   RelativeEncoder encleftb;
   RelativeEncoder encrightb;
+  Encoder leftEncoder;
+  Encoder rightEncoder;
 
   public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
   // private static final int kCPR = 8192;
@@ -56,7 +69,17 @@ public class DriveTrain extends SubsystemBase {
   DifferentialDriveKinematics kinematics;
   PIDController anglePidController;
   CANSparkMax intake;
-  
+  private final  double kEncoderTicktoMeter = 1.0 / 4096.0 * 0.128 * Math.PI;
+  // The gyro sensor
+  private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+  // These classes help us simulate our drivetrain
+  public DifferentialDrivetrainSim m_drivetrainSimulator;
+  private EncoderSim leftEncoderSim;
+  private EncoderSim rightEncoderSim;
+  // The Field2d class shows the field in the sim GUI
+  private Field2d m_fieldSim;
+  private ADXRS450_GyroSim m_gyroSim;
  
 
   /** Creates a new DriveTrain. */
@@ -70,13 +93,19 @@ public class DriveTrain extends SubsystemBase {
     // rightmotorfront.setInverted(true);
     rightmotorback=new CANSparkMax(21,  MotorType.kBrushless);
     // rightmotorback.setInverted(true);
+
+    leftEncoder = new Encoder(0, 1);
+    rightEncoder = new Encoder(2, 3);
+
     leftmotors=new MotorControllerGroup(leftmotorfront,leftmotorback);
     rightmotors=new MotorControllerGroup(rightmotorfront,rightmotorback);
+
     drive=new DifferentialDrive(leftmotors, rightmotors);
     drive.setDeadband(0.05);
 
     // leftmotorfront.restoreFactoryDefaults();
     encleftf = leftmotorfront.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+
 
     // rightmotorfront.restoreFactoryDefaults();
     encrightf = rightmotorfront.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
@@ -104,9 +133,28 @@ public class DriveTrain extends SubsystemBase {
     leftmotorfront.setInverted(false);
     rightmotorfront.setInverted(true);
 
-    
-    
-  
+    // Sets the distance per pulse for the encoders
+    leftEncoder.setDistancePerPulse(DriveConstants.kEncoderTicktoMeter);
+    rightEncoder.setDistancePerPulse(DriveConstants.kEncoderTicktoMeter);
+    resetEncoders();
+    if (RobotBase.isSimulation()) { // If our robot is simulated
+      // This class simulates our drivetrain's motion around the field.
+      m_drivetrainSimulator =
+          new DifferentialDrivetrainSim(
+              DriveConstants.kDrivetrainPlant,
+              DriveConstants.kDriveGearbox,
+              DriveConstants.kDriveGearing,
+              DriveConstants.kTrackwidthMeters,
+              DriveConstants.kWheelDiameterMeters / 2.0,
+              VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));
+
+      // The encoder and gyro angle sims let us set simulated sensor readings
+      leftEncoderSim = new EncoderSim(leftEncoder);
+      rightEncoderSim = new EncoderSim(rightEncoder);
+    }
+  }
+  double getEncoderMeters() {
+    return (leftEncoder.get() * -rightEncoder.get())/2 * kEncoderTicktoMeter;
   }
 
   @Override
@@ -119,7 +167,9 @@ public class DriveTrain extends SubsystemBase {
     // SmartDashboard.putNumber("Y", pose.getY());
     SmartDashboard.putNumber("Left", encleftf.getPosition() * Math.PI * Units.inchesToMeters(6)/7.);//
     SmartDashboard.putNumber("Right", encrightf.getPosition() * Math.PI * Units.inchesToMeters(6)/7.31);
-    
+    m_gyro.getAngle();
+    m_gyroSim = new ADXRS450_GyroSim(m_gyro);
+    m_fieldSim.setRobotPose(getPose());
   }
   public Rotation2d getHeading(){
     return Rotation2d.fromDegrees(-ahrs.getAngle());
@@ -153,7 +203,7 @@ public double rightencpos(){
     encrightb.setPosition(0);
   }
   public double orient(double angle){
-    pose=odom_drive.update(getHeading(),leftencpos(),rightencpos());
+   pose=odom_drive.update(getHeading(),leftencpos(),rightencpos());
    leftmotors.set( -anglePidController.calculate(pose.getRotation().getDegrees(), angle)/250);
    rightmotors.set(anglePidController.calculate(pose.getRotation().getDegrees(), angle)/250);
    return (angle-pose.getRotation().getDegrees());
@@ -167,6 +217,33 @@ public double rightencpos(){
   }
   public void res(){
     ahrs.reset();
+  }
+  public void resetEncoders() {
+    leftEncoder.reset();
+    rightEncoder.reset();
+  }
+  // Returns the currently-estimated pose of the robot.
+  public Pose2d getPose() {
+    return odom_drive.getPoseMeters();
+  }
+
+
+  @Override
+  public void simulationPeriodic() {
+    // To update our simulation, we set motor voltage inputs, update the simulation,
+    // and write the simulated positions and velocities to our simulated encoder and gyro.
+    // We negate the right side so that positive voltages make the right side
+    // move forward.
+    m_drivetrainSimulator.setInputs(
+        leftmotors.get() * RobotController.getBatteryVoltage(),
+        rightmotors.get() * RobotController.getBatteryVoltage());
+    m_drivetrainSimulator.update(0.020);
+
+    leftEncoderSim.setDistance(m_drivetrainSimulator.getLeftPositionMeters());
+    leftEncoderSim.setRate(m_drivetrainSimulator.getLeftVelocityMetersPerSecond());
+    rightEncoderSim.setDistance(m_drivetrainSimulator.getRightPositionMeters());
+    rightEncoderSim.setRate(m_drivetrainSimulator.getRightVelocityMetersPerSecond());
+    m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
   }
 
 }
